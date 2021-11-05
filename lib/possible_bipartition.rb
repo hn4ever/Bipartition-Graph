7f88e91d1fdd8bcc8b1810398cf81c0aa7ab1082
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-11-04 18:23:17 -0700

    finalised and passed all tests

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,70 +1,26 @@
 
 def possible_bipartition(dislikes)
+    color = Array.new(dislikes.size, 0)
+    stack =[]
 
-
-    # color = Array.new(dislikes.size, nil) # each dog will have a specific color
-
-    # dislikes.each_with_index do |dog, i|
-    #     if color[i].nil?
-    #         color[i] = 0
-    #         if !dog.empty?
-    #             dog.each do |enemies|
-    #                 stack = [] # first dog is dog 0
-    #                 stack << 0
-    #                 until stack.empty?
-    #                     current = stack.pop
-    #                     dislikes[current].each do |enemy|
-    #                         if color[enemy].nil?
-    #                             color[enemy] = -color[i]
-    #                             stack << dislikes[enemy]
-    #                         elsif color[enemy] == color[i]
-    #                             false
-    #                         end
-    #                     end
-    #                 end
-    #             end
-    #         end
-    #     end
-    # end
-    # true
-
-    # color the dog green = 1
-    # dfs if it's not colored
-    #   color the different color color[enemy] = - color[dog]
-    # if it's colord and the color is the opposite and colored, continue
-    # if color the same then return false
-
-    color = Array.new(dislikes.size, nil)
-
-    dislikes.each_with_index do |enemies, i|
-        if !color[i].nil?
-            stack = [i]
+    dislikes.each_index do |i|
+        if color[i] == 0
+            stack << i
             color[i] = 1
             until stack.empty?
-                if !enemies.empty?
-                    enemies.each do |enemy|
-                        if color[ememy].nil? #color first then add to stack?
-                            color[enemy] = -color[i]
+                dog = stack.pop
+                if !dislikes[dog].empty?
+                    dislikes[dog].each do |enemy|
+                        if color[enemy] ==  0
+                            color[enemy] = -1 * (color[dog])
                             stack << enemy
-                        else
-                            return false if color[enemy] ==  color[i]
+                        elsif color[enemy] == color[dog]
+                            return false
                         end
                     end
                 end
             end
         end
     end
-    true
+    return true
 end
-
-
-dislikes = [ [3, 6],
-[2, 5],
-[1, 3],
-[0, 2],
-[5],
-[1, 4],
-[0]
-]
-
-puts possible_bipartition(dislikes)
